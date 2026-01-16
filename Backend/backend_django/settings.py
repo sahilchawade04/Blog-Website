9aea0858d6f86d7d4a38ca2fa83216c8b859bdb7
sahilchawade04 <sahilchawade04@gmail.com>
2026-01-16 12:20:35 +0530

    Update Settings.py

--- Backend/backend_django/settings.py
+++ Backend/backend_django/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', localhost),
-        'PORT': os.getenv('DB_PORT', 3306),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
